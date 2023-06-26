d43d4933804b6cfcab6971806052da040df27ed1
Sebastian <admin@MacBook-Pro-de-Usuario.local>
2023-06-26 16:32:44 -0400

    se esta consumiendo la api de climas y un mock de comunas de chile

--- climas-angular/src/app/app.component.ts
+++ climas-angular/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'climas-angular';
+export class AppComponent implements OnInit {
+
+  ciudades: any;
+
+  constructor(
+    private services: WeatherService,
+    private http: HttpClient
+  ){}
+
+  ngOnInit(): void {
+      this.services.getWeather('santiago','cl').subscribe(resp => {
+        console.log('DATA', resp)
+      },
+      err => console.log('error', err));
+
+      this.services.getCities().subscribe(resp => {
+        this.ciudades = resp.data;
+      })
+  }
 }
---
+++ climas-angular/src/app/services/weather.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  apiKey: string;
+  URL: string;
+
+  constructor(private http: HttpClient) { 
+    this.apiKey = '6f06e3f730dda087ba4bc33574ddd934';
+    this.URL = 'https://api.openweathermap.org/data/2.5/weather?appid=' + this.apiKey + '&q=';
+  }
+
+  getWeather(cityName: string, countryCode: string){
+    return this.http.get(this.URL + cityName + ',' + countryCode); 
+  }
+  public getCities() :Observable<any>{
+    return this.http.get<any>(
+      'assets/mocks/cities-mock.json'
+      );
+  }
+
+}
